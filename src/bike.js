9b204f63628ea1ad0332a9f5ed1c463a5e0ef23d
Guest User <Guest@epicodus-9.local>
2018-08-08 11:31:54 -0700

    Web App the uses an API to filter lost or stolen bikes

---
+++ src/bike.js
@@ -0,0 +1,18 @@
+export class BikeSearch {
+  findBikeByCity(city, distance) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=${city}&distance=${distance}&stolenness=stolen`;
+
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,28 @@
-//logic for user interface
-
 import './styles.css';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BikeSearch } from './bike.js';
+
+$(document).ready(function() {
+  $('#stolenBike').click(function() {
+    let city = $('#location').val();
+    $('#location').val("");
+    let distance = $('#distance').val();
+    $('#distance').val("");
+
+    let bikes = new BikeSearch();
+    let promise = bikes.findBikeByCity(city, distance);
+
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      for (let i = 0; i < body.bikes.length; i++) {
+         $('.title').append(`<li>Stolen bike: ${body.bikes[i].title}</li>`);
+         if ( `${body.bikes[i].large_img}` != "null") {
+           $('.title').append(`<img src ='${body.bikes[i].large_img}'/>`);
+
+         }
+      }
+      });
+  });
+});
